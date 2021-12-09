b49a4329bc79f1e3fa3fef85034f40a047946fa7
qiyeu.zhen@boonray.com <qiyeu.zhen@boonray.com>
2021-12-09 14:38:06 +0800

    合并

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+<<<<<<< HEAD
 import './App.css'
 import { IndexedDb } from './IndexedDb/IndexedDb'
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> 961a332... Initialize project using Create React App
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <IndexedDb></IndexedDb>
     </div>
   )
 }
 
 export default App
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 961a332... Initialize project using Create React App
